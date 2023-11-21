b314dd927cf9d9d1dbec84636791dd210d963ab3
Ankor <ankor_socialki@mail.ru>
2023-11-21 12:43:57 +0300

    -add crop and scaleAspectFill

--- SMUIK_7/ViewController.swift
+++ SMUIK_7/ViewController.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     private lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.delegate = self
         scrollView.contentInsetAdjustmentBehavior = .never
         scrollView.frame = view.bounds
         scrollView.scrollIndicatorInsets = UIEdgeInsets(
-            top: 270 - view.safeAreaInsets.top,
+            top: 270 - view.safeAreaInsets.top ,
             left: 0,
             bottom: 0,
             right: 0
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "marathonImage")
-        imageView.contentMode = .scaleToFill
+        imageView.contentMode = .scaleAspectFill
         imageView.frame = CGRect(
             x: 0,
             y: 0,
@@ -42,37 +42,48 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.addSubview(scrollView)
+        croppingImage(imageView: &imageView)
         scrollView.addSubview(imageView)
+    }
+    
+    private func croppingImage( imageView: inout UIImageView) {
+        if let croppedImage = imageView.croppedImage() {
+            let newImageView = UIImageView(image: croppedImage)
+            newImageView.contentMode = .scaleAspectFill
+            imageView = newImageView
+        }
     }
 }
 
 extension ViewController: UIScrollViewDelegate {
-    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offsetY = scrollView.contentOffset.y
-
+        
         if offsetY < 0 {
-             imageView.frame = CGRect(
+            imageView.frame = CGRect(
                 x: 0,
                 y: offsetY,
                 width: view.bounds.width,
                 height: 270 - offsetY
             )
-            imageView.bounds = CGRect(
-               x: 0,
-               y: 0,
-               width: view.bounds.width - offsetY,
-               height: 270 - offsetY
-           )
-            
             scrollView.scrollIndicatorInsets = UIEdgeInsets(
-                top: 270 - offsetY - view.safeAreaInsets.top,
+                top: 270 - offsetY - view.safeAreaInsets.top ,
                 left: 0,
                 bottom: 0,
                 right: 0
             )
         }
+    }
+}
+
+extension UIImageView {
+    func croppedImage() -> UIImage? {
+        let renderer = UIGraphicsImageRenderer(size: bounds.size)
+        let image = renderer.image { context in
+            layer.render(in: context.cgContext)
+        }
+        return image
     }
 }
